7de1fb6c2fe70f3ec2b856594b3575e4250210e6
pritam6291 <122303417+pritam6291@users.noreply.github.com>
2023-09-25 18:00:00 +0530

    Update Example_code.java

--- src/Example_code.java
+++ src/Example_code.java
@@ -2,7 +2,7 @@
 public class Example_code {
 
 	public static void main(String[] args) {
-		System.out.println("Working on assignment");
+		System.out.println("Working on assignment"+"adding new code changes");
 
 	}
 
